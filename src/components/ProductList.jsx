474299513f8cae9ad9cbc8a8e23ab546c22001e5
Rahul Pal <pal_rahul@rudrainnovative.in>
2021-09-08 16:23:21 +0530

    change in navbar

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,16 +3,18 @@ import Product from "./Product";
 import Title from "./Title";
 import { ProductContext } from "../Context";
 export default function ProductList() {
-  const products= useContext(ProductContext);
+  const products = useContext(ProductContext);
   return (
     <>
       <div className="py-5">
         <div className="container">
           <Title name="our" title="products" />
-          
+
           <div className="row">
             {products &&
-              products?.products.map((u, i) => <Product product={u} key={u.id} />)}
+              products?.products.map((u, i) => (
+                <Product product={u} key={u.id} />
+              ))}
           </div>
         </div>
       </div>
